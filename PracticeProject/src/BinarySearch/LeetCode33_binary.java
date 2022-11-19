284765f259834dcb976adde7b1352cf88ba74555
KiranJadhav <kiran.jadhav1993@gmail.com>
2022-11-19 15:02:47 +0530

    19-11 updated

--- PracticeProject/src/BinarySearch/LeetCode33_binary.java
+++ PracticeProject/src/BinarySearch/LeetCode33_binary.java
@@ -5,8 +5,8 @@ public class LeetCode33_binary {
 	public static void main(String[] args) {
 		int []arr= {4,5,6,7,1,2,3};
 
-		search(arr,0);
-		System.out.println(search(arr,6));
+		
+		System.out.println(search(arr,3));
 
 	}
 
@@ -21,11 +21,11 @@ public class LeetCode33_binary {
 		}
 
 		//if pivot is found
-		if(nums[pivot]==target) {
+		else	if(nums[pivot]==target) {
 			return pivot;
 		}
 
-		if(target>=nums[0]) {
+		else	if(target>=nums[0]) {
 			return binarySearch(nums, target, 0, pivot-1);
 		}
 		else {
@@ -59,14 +59,14 @@ public class LeetCode33_binary {
 			if(mid<end && arr[mid]>arr[mid+1]) {
 				return mid;
 			}
-			if(mid>start && arr[mid]<arr[mid-1]) {
+			else	if(mid>start && arr[mid]<arr[mid-1]) {
 				return mid-1;
 			}
-			if(arr[mid]<=arr[start]) {
+			else	if(arr[mid]<=arr[start]) {
 				end=mid-1;
 			}
 			else {  //start<=mid
-				start=mid+1;
+				start=mid+1; //{3,4,5,6,7,0,1,2}
 			}
 		}
 		return -1;
